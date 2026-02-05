6948d34eef5eac7ed6c290a3f1cf3d1f6c318fa9
Adrien Cominotto <adrien.cominotto@gmail.com>
2026-02-05 09:05:42 +0100

    Update package version to 0.0.3, add tsconfig.build.tsbuildinfo to .gitignore, and enhance exports in index.ts for better type definitions.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+export {
+    ForceRetryError, HTTPError, isForceRetryError,
+    isHTTPError,
+    isKyError,
+    isTimeoutError, TimeoutError, type AfterResponseHook, type AfterResponseState, type BeforeErrorHook, type BeforeErrorState, type BeforeRequestHook, type BeforeRequestState,
+    type BeforeRetryHook, type BeforeRetryState, type Hooks,
+    type Input, type KyInstance, type KyRequest, type KyResponse, type NormalizedOptions, type Options, type Progress, type ResponsePromise, type RetryOptions,
+    type SearchParamsOption,
+    type ShouldRetryState
+} from 'ky'
 export * from './zod-ky'
-export { HTTPError } from 'ky'
+export type { AugmentedKyInstance, AugmentedResponse, AugmentedResponsePromise } from './zod-ky.types'
+
+
